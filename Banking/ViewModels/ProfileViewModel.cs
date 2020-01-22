f9e57a5b6421fde1d6ff4f317733246a5c6764be
jinming liu <s3596621@student.rmit.edu.au>
2020-01-22 18:00:38 +1100

    create the Profile page

---
+++ Banking/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Banking.Models;
+
+namespace Banking.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public ProfileViewModel()
+        {
+        }
+        public Customer Customer { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostCode { get; set; }
+        public string Phone { get; set; }
+        public string TFN { get; set; }
+    }
+}
