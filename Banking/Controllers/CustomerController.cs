6ee6d9b5f538ea0b6f6c8c6fea73540755a4888d
Shiyu <s3734720@student.rmit.edu.au>
2020-01-22 17:40:46 +1100

    withdraw post controller

--- Banking/Controllers/CustomerController.cs
+++ Banking/Controllers/CustomerController.cs
@@ -45,6 +45,40 @@ namespace Banking.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Withdraw(WithdrawViewModel viewModel)
+        {
+            var customer = await _context.Customer.FindAsync(CustomerID);
+            viewModel.Customer = customer;
+
+            if (viewModel.Amount <= 0)
+            {
+                ModelState.AddModelError("Amount", "Amount must be greater than zero.");
+                viewModel.OperationStatus = OperationStatus.Failed;
+                return View(viewModel);
+            }
+
+            var account = customer.Accounts.Find(x => x.AccountType == viewModel.AccountType);
+            if (viewModel.Amount > account.Balance)
+            {
+                ModelState.AddModelError("Amount", "Amount exceeds current balance.");
+                viewModel.OperationStatus = OperationStatus.Failed;
+                return View(viewModel);
+            }
+
+            account.Balance -= viewModel.Amount;
+            account.Transactions.Add(new Transaction
+            {
+                TransactionType = TransactionType.Deposit,
+                Amount = viewModel.Amount,
+                ModifyDate = DateTime.UtcNow
+            });
+            await _context.SaveChangesAsync();
+
+            viewModel.OperationStatus = OperationStatus.Successful;
+            return View(viewModel);
+        }
+
         public async Task<IActionResult> Deposit()
         {
             var customer = await _context.Customer.FindAsync(CustomerID);
