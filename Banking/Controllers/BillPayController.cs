d226520ddb260eb39143e796ecddf54469cb02c8
Shiyu <s3734720@student.rmit.edu.au>
2020-01-24 11:54:18 +1100

    bill pay controller init

---
+++ Banking/Controllers/BillPayController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+using Banking.Attributes;
+using Banking.Data;
+using Banking.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Banking.Controllers
+{
+    [AuthorizeCustomer]
+    [Route("Customer/BillPay")]
+    public class BillPayController : Controller
+    {
+        private readonly BankingContext _context;
+
+        private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+        public BillPayController(BankingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
