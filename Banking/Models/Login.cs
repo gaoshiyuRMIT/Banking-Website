e0d3f6b339cd717a6ae1e85fc691f32f94261ba3
jinming liu <s3596621@student.rmit.edu.au>
2020-01-24 17:43:01 +1100

    Login constrant

--- Banking/Models/Login.cs
+++ Banking/Models/Login.cs
@@ -8,14 +8,22 @@ namespace Banking.Models
         [Key]
         [StringLength(8)]
         [Display(Name = "User ID")]
+        //[DataType(DataType.PhoneNumber, ErrorMessage = "Not a number"), StringLength(8)]
+        //[RegularExpression("[^0-9]", ErrorMessage = "User ID must be numeric"), StringLength(8)]
+        [RegularExpression(@"^\d{8}", ErrorMessage = "Only  Numbers allowed.")]
+        [Required(AllowEmptyStrings = false)]
         public string UserID { get; set; }
 
         [Required, StringLength(64)]
         public string PasswordHash { get; set; }
 
+        [StringLength(4)]
+        [Required(AllowEmptyStrings = false)]
         public int CustomerID { get; set; }
         public virtual Customer Customer { get; set; }
 
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        [Required(AllowEmptyStrings = false)]
         public DateTime ModifyDate { get; set; }
     }
 }
