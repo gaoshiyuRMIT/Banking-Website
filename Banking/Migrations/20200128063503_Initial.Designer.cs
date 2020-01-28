89e0a45c53ef435a141697e777ad18c01b1b5f9c
Shiyu <s3734720@student.rmit.edu.au>
2020-01-28 17:48:26 +1100

    update migrations

--- Banking/Migrations/20200128063503_Initial.Designer.cs
+++ Banking/Migrations/20200128063503_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Banking.Migrations
 {
     [DbContext(typeof(BankingContext))]
-    [Migration("20200128060901_Initial")]
+    [Migration("20200128063503_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
