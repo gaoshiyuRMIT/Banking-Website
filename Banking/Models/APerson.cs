ba3110ca2de936f7a8c8d89fb4e254bf587d6c20
jinming liu <s3596621@student.rmit.edu.au>
2020-01-28 13:46:17 +1100

    constrant A Person

--- Banking/Models/APerson.cs
+++ Banking/Models/APerson.cs
@@ -7,12 +7,37 @@ namespace Banking.Models
 {
     public abstract class APerson
     {
-        [Required]
+        [Display(Name = "Name")]
+        [Required, StringLength(50, ErrorMessage = "Please enter 50 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only  letter allowed.")]
         public string Name { get; set; }
+
+
+        [Display(Name = "Address")]
+        [StringLength(50, ErrorMessage = "Please enter no more than 50 characters")]
         public string Address { get; set; }
+
+        [Display(Name = "City")]
+        [StringLength(40, ErrorMessage = "Please enter no more than 40 characters")]
         public string City { get; set; }
+
+        [Display(Name = "State")]
+        [StringLength(3, ErrorMessage = "Please enter 3 lettered Australian state")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Only  letter allowed.")]
         public string State { get; set; }
+
+
+        [Display(Name = "Post Code")]
+        [StringLength(4, ErrorMessage = "Please enter 4 digit number")]
+        [RegularExpression(@"^\d{4}", ErrorMessage = "Only  Numbers allowed.")]
         public string PostCode { get; set; }
+
+
+        [Display(Name = "Phone")]
+        //[Required(ErrorMessage = "You must provide a phone number")]
+        //[DataType(DataType.PhoneNumber)]
+        //[RegularExpression(@"^\(\+61)[-. ]([0-9]{9})$", ErrorMessage = "Not a valid phone number")]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:61-XXXXXXXXX}")]
         public string Phone { get; set; }
     }
 }
